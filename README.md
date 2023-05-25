fb8f4d682b7304daddedbdf005f388cd240f5fee
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2023-05-25 15:45:52 +0000

    :tada: Prototyping

---
+++ README.md
@@ -0,0 +1,10 @@
+# Maroon Europium Vedalia
+
+A Chrome extension to build script for youtube-dl from context menu.
+
+## Features
+
+- Add download script for video
+- Add download script for video but audio only
+- Clear saved scripts
+- Export and clear saved scripts
---
+++ scripts/background.js
@@ -0,0 +1,88 @@
+const videoScript = (vid) =>
+  `C:/Users/klein_private/scoop/apps/python/current/Scripts/youtube-dl.exe -o "%%(channel_id)s-%%(channel)s/%%(title)s-%%(id)s.%%(ext)s" --external-downloader aria2c -f bestaudio[ext=m4a],bestvideo[ext=mp4] --add-metadata --xattrs --write-thumbnail --embed-thumbnail https://youtu.be/${vid}`;
+
+const musicScript = (vid) =>
+  `C:/Users/klein_private/scoop/apps/python/current/Scripts/youtube-dl.exe -o "%%(channel_id)s/%%(title)s.%%(ext)s" --external-downloader aria2c -f bestvideo[ext=mp4] --add-metadata --xattrs --write-thumbnail --embed-thumbnail https://youtu.be/${vid}`;
+
+const tabUrl = (info, tab) => tab.url;
+const linkUrl = (info, tab) => info.linkUrl;
+const extractVID = (page) => new URL(page).searchParams.get("v");
+
+const builder = (url) => (template) => (info, tab) => {
+  const vid = extractVID(url(info, tab));
+  return chrome.storage.local.get("script").then((old) =>
+    chrome.storage.local.set({
+      script: (old.script ?? "") + template(vid) + "\n",
+    })
+  );
+};
+
+const exportScripts = () =>
+  chrome.storage.local
+    .get("script")
+    .then((v) =>
+      v.script
+        ? chrome.downloads.download({
+            url: "data:text/bat;base64," + btoa(v.script),
+            filename: "download-yt.bat",
+          })
+        : chrome.notifications.create({
+            iconUrl: "/assets/images/icon48.png",
+            title: "YouTube-DL Builder",
+            message: "Nothing to be exported",
+            type: "basic",
+          })
+    )
+    .then(() => chrome.storage.local.clear());
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "video",
+    title: "Download this video",
+    contexts: ["page"],
+    documentUrlPatterns: ["https://*.youtube.com/watch?v=*"],
+  });
+  chrome.contextMenus.create({
+    id: "audio",
+    title: "Download this video as music",
+    contexts: ["page"],
+    documentUrlPatterns: ["https://*.youtube.com/watch?v=*"],
+  });
+
+  chrome.contextMenus.create({
+    id: "video-link",
+    title: "Download the linked video",
+    contexts: ["link"],
+    documentUrlPatterns: ["https://*.youtube.com/*"],
+  });
+  chrome.contextMenus.create({
+    id: "audio-link",
+    title: "Download the linked video as music",
+    contexts: ["link"],
+    documentUrlPatterns: ["https://*.youtube.com/*"],
+  });
+
+  chrome.contextMenus.create({
+    id: "clear",
+    title: "Clear saved scripts",
+  });
+  chrome.contextMenus.create({
+    id: "export",
+    title: "Export and clear all saved scripts",
+  });
+});
+
+const handlers = {
+  video: builder(tabUrl)(videoScript),
+  audio: builder(tabUrl)(musicScript),
+  "video-link": builder(linkUrl)(videoScript),
+  "audio-link": builder(linkUrl)(musicScript),
+  clear: () => chrome.storage.local.clear(),
+  export: exportScripts,
+};
+
+chrome.contextMenus.onClicked.addListener((info, tab) =>
+  handlers[info.menuItemId](info, tab)
+);
+
+chrome.action.onClicked.addListener((tab) => exportScripts());
